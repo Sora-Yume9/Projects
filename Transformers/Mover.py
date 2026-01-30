b8ce6f6dbee4dad0a0f41a1babadcf6917d306b9
Sora-Yume9 <contacts.alihassan@gmail.com>
2026-01-30 13:17:25 +0500

    Add files via upload

---
+++ Transformers/Mover.py
@@ -0,0 +1,8 @@
+with open(r"C:\Projects\test.txt","r",encoding="utf-8") as f:
+    F = f.read()
+
+with open(r"C:\Projects\Transformers\combined.txt","r",encoding="utf-8") as m:
+    k = m.read()
+
+print(len(k))
+
